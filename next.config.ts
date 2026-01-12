8e6f4967b6c57f7f71941d737b84093072384e47
alinaseem123 <alinaseem21102002@gmail.com>
2026-01-12 16:45:19 +0500

    chore: configure image domains and update project dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
   reactCompiler: true,
+
   images: {
+    domains: [
+      "ykbzvxnqnlidvmxpkonv.supabase.co",
+    ],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
       },
     ],
   },
